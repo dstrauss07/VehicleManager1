a664f49b802ecdba96c5f26c277cd16f53e840df
dstrauss <fmdenver@gmail.com>
2018-12-06 19:52:12 -0500

    Initial Project Creation

---
+++ src/VehicleManager1/VehicleManager1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleManager1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
